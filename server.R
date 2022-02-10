a94912ea60d64f6ec3f6982c22bf00f245abe94c
Cole Bruss <98360569+cllbruss@users.noreply.github.com>
2022-02-10 08:51:36 -0500

    fixed autoplot to region

--- server.R
+++ server.R
@@ -1,16 +1,12 @@
 server = function(input, output) {
   output$debug <- renderPrint({
-    input$date
+    input$region
   })
   
   output$reg <- renderPlot({
-    min_date <- input$date[1]
-    max_date <- input$date[2]
     
-    reg_plot <- tourism[input$date >= min_date,]
-    reg_plot <- tourism[input$date <= max_date,]
-    
-    autoplot(reg_plot)
+    plot_df <- tourism[tourism$Region == input$region,]
+    autoplot(plot_df)
     
   })
 }
--- ui.R
+++ ui.R
@@ -4,14 +4,7 @@ ui <- fluidPage(
     label = "Select Region:",
     choices = unique(tourism$Region),
   ),
-  dateRangeInput(
-    inputId = "date",
-    label = "Select Date:",
-    min = min(tourism$Quarter),
-    max = max(tourism$Quarter),
-    start = min(tourism$Quarter),
-    end = max(tourism$Quarter)
-  ),
+
   plotOutput("reg"),
   verbatimTextOutput("debug")
 )
